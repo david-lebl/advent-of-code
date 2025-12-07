d59731b88fbbcfd30862ef0da6d6bbd3473b1bd2
David Lebl <david.lebl@adastragrp.com>
2025-12-07 20:24:17 +0100

    feat: day4 in scala - part2

--- 2025/scala/day4.scala
+++ 2025/scala/day4.scala
@@ -1,18 +1,21 @@
 package day4
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 type Grid = Array[Array[Char]]
 extension (grid: Grid)
   def isDefinedAt(x: Int, y: Int): Boolean =
     x >= 0 && y >= 0 && y < grid.length && x < grid(y).length
+  def updatedAt(x: Int, y: Int)(c: Char): Grid =
+    grid.updated(y, grid(y).updated(x, c))
 
 object Grid:
   def fromString(s: String): Grid =
     s.linesIterator.map(_.toCharArray).toArray
 
-def program(grid: Grid): Long =
-  (for
+def findNeighbours(grid: Grid): Seq[(x: Int, y: Int, cnt: Long)] =
+  for
     y <- grid.indices
     x <- grid(y).indices
     if grid(y)(x) == '@'
@@ -24,9 +27,21 @@ def program(grid: Grid): Long =
         if (x, y) != (nx, ny)
       yield grid(ny)(nx)
     cnt = papers.count(_ == '@')
-  yield cnt).count(_ < 4)
+  yield (x = x, y = y, cnt = cnt)
 
-def program(matrix: String): Long = program(Grid.fromString(matrix))
+def program_part1(grid: Grid): Long = findNeighbours(grid).count(_.cnt < 4)
+def program_part1(matrix: String): Long = program_part1(Grid.fromString(matrix))
+
+def program_part2(matrix: String): Long =
+  program_part2(Grid.fromString(matrix), 0)
+@tailrec
+def program_part2(grid: Grid, totalCnt: Int): Long =
+  val removing = findNeighbours(grid).filter(_.cnt < 4)
+  if removing.isEmpty then totalCnt
+  else
+    val updated = removing.foldLeft(grid): (g, r) =>
+      g.updatedAt(r.x, r.y)('.')
+    program_part2(updated, totalCnt + removing.size)
 
 val exampleData =
   """..@@.@@@@.
@@ -44,21 +59,19 @@ val exampleData =
 lazy val fullData = Source.fromFile("../inputs/day4_input.txt").mkString
 
 @main def part1_example() =
-  val res = program(exampleData)
+  val res = program_part1(exampleData)
   println(s"Result: $res")
   assert(res == 13)
 
 @main def part1() =
-  val res = program(fullData)
+  val res = program_part1(fullData)
   println(s"Result: $res")
 
 @main def part2_example() =
-  ???
-  val res = program(exampleData)
+  val res = program_part2(exampleData)
   println(s"Result: $res")
-//  assert(res == ???)
+  assert(res == 43)
 
 @main def part2() =
-  ???
-  val res = program(fullData)
+  val res = program_part2(fullData)
   println(s"Result: $res")
